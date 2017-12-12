85ce068233b08a0eb7c86b0c77b7dbf05ba4ef18
Sai Kishore <sai2009kishore@gmail.com>
2017-12-12 06:39:24 +0000

    Update Sandwich.java

--- Sandwich.java
+++ Sandwich.java
@@ -1,4 +1,34 @@
 public class Sandwich
 {
+  public String getSandwich(String str) 
+  {
+    int cnt = count(str, "bread");
+    if(cnt <=1)
+      return "";
 
+    return rev(breakIt(rev(breakIt(str, "bread")), "daerb"));
+  }
+
+  public String breakIt(String str, String sub)
+  {
+    int in = str.indexOf(sub);
+    return str.substring(in+sub.length(), str.length());
+  }
+
+  public String rev(String str)
+  {
+    String re = "";
+    for(int i=str.length()-1;i>=0;i--)
+      re += str.charAt(i)+"";
+    return re;
+  }
+
+  public int count(String str, String sub)
+  {
+    int count = 0;
+    for(int i=0;i<str.length()-4;i++)
+      if(str.substring(i, i+5).equals("bread"))
+        count++;
+    return count;
+  }
 }
