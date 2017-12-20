4f3a51f1b25b675a40aca716c31e963a48d99da7
Sai Kishore <sai2009kishore@gmail.com>
2017-12-20 12:12:57 +0530

    Update BigSum.java

--- BigSum.java
+++ BigSum.java
@@ -1,70 +1,188 @@
-public class BigSum
+import java.util.Scanner;
+
+/**
+ * Calculates sum and product of large numbers
+ * Created by Sai Kishore on 12/20/2017.
+ */
+
+public class BigMultiply
 {
-    public static void main(String args[]) 
-    {
-        String a = args[0];
-        String b = args[1];
-        add(a, b);
-    }
-    
-    public static void add(String a, String b)
-    {
-        int A = a.length();
-        int B = b.length();
-        int diff = Math.abs(A-B);
-        int max = A>B?A:B;
-        if(diff!=0)
-        {
-            String head = "";
-            for(int i=0;i<diff;i++)
-                head+="0";
-            if(A>B)
-                b = head+b;
-            else
-                a = head+a;
-        }
-        
-        int sum[] = new int[max+1];
-        for(int i=0;i<sum.length;i++)
-            sum[i] = 0;
-        
-        a = rev(a);
-        b = rev(b);
+	// main method
+	public static void main(String args[])
+	{
+		Scanner scanner = new Scanner(System.in);
+		String firstNum = scanner.next();
+		String secondNum = scanner.next();
+		p(multiply(firstNum, secondNum));
+	}
+
+	/*
+	* Multiplies the two numbers digit by digit
+	* @param firstNum - The first number
+	* @param secondNum - the second number
+	* @return - Product of the given numbers
+	*/
+	private static String multiply(String firstNum, String secondNum)
+	{
+		String result = "0";
+		if(firstNum.equals("0") || secondNum.equals("0"))
+			return "0";
+		if(firstNum.equals("1"))
+			return secondNum;
+		if(secondNum.equals("1"))
+			return firstNum;
+		int length1 = firstNum.length();
+		int length2 = secondNum.length();
+		String max = length1>length2 ? firstNum : secondNum;
+		String min = length1>length2 ? secondNum : firstNum;
+
+		max = rev(max);
+		min = rev(min);
+
+		length1 = max.length();
+		length2 = min.length();
+
+		String muls[] = new String[length2];
+		for(int i=0;i<muls.length;i++)
+			muls[i] = "";
 
 
-        for(int i=0;i<max;i++)
-        {
-            int temp = toInt(a, i) + toInt(b, i) + sum[i];
-            
-            if(temp > 9)
-            {
-                sum[i+1] = temp/10;
-                temp %= 10;
-            }
-            
-            sum[i] = temp;
-        }
+		for(int i=0;i<length2;i++)
+		{
+			int carry[] = new int[length1+1];
+			for(int c=0;c<carry.length;c++)
+				carry[c] = 0;
+			int temp;
+			for(int j=0;j<length1;j++)
+			{
+				temp = ( toInt(max, j) * toInt(min, i) ) + carry[j];
+				if(temp > 10)
+				{
+					carry[j+1] = temp/10;
+					temp = temp % 10;
+				}
+				muls[i] += temp;
+			}
+			muls[i] = getZeros(i) + muls[i] + carry[length1];
+		}
 
-        for(int i=max;i>=0;i--)
-            System.out.print(sum[i]);
-    }
-    
+		for(int i=0;i<muls.length;i++)
+			muls[i] = rev(muls[i]);
 
-    public static int toInt(String s, int i)
-    {
-        return Integer.parseInt(s.charAt(i)+"");
-    }
-    
-    public static String rev(String org)
-    {
-        String ret = "";
-        for(int i=org.length()-1;i>=0;i--)
-            ret += org.charAt(i)+"";
-        return ret;
-    }
-    
-    public static <E> void p(E e)
-    {
-        System.out.println(e);
-    }
+		for(String mul : muls)
+			result = add(result, mul);
+
+		return trim(result);
+	}
+
+	/*
+	* Provides the specified number of zeroes in string form
+	* @param num - The required number of zeroes
+	* @return - The string with num zeroes
+	*/
+	private static String getZeros(int num)
+	{
+		String temp = "";
+		for(int i=0;i<num;i++)
+			temp += "0";
+		return temp;
+	}
+
+	/*
+	* Adds two numbers digit by digit
+	* @param a - The first number
+	* @param b - The second number
+	* @return - the sum of two numbers
+	*/
+	private static String add(String a, String b)
+	{
+		String result = "";
+		int A = a.length();
+		int B = b.length();
+		int diff = Math.abs(A-B);
+		int max = A>B?A:B;
+		if(diff!=0)
+		{
+			String head = getZeros(diff);
+			if(A>B)
+				b = head+b;
+			else
+				a = head+a;
+		}
+
+		int sum[] = new int[max+1];
+		for(int i=0;i<sum.length;i++)
+			sum[i] = 0;
+
+		a = rev(a);
+		b = rev(b);
+
+
+		for(int i=0;i<max;i++)
+		{
+			int temp = toInt(a, i) + toInt(b, i) + sum[i];
+
+			if(temp > 9)
+			{
+				sum[i+1] = temp/10;
+				temp %= 10;
+			}
+
+			sum[i] = temp;
+		}
+
+		for(int i=max;i>=0;i--)
+			result += sum[i];
+
+		return trim(result);
+	}
+
+	/*
+	* Converts the char at a particular location in a string to int
+	* @param s - The string whose char is tp be converted
+	* @param i - The position of the character
+	* @return - The number at given position
+	*/
+	private static int toInt(String s, int i)
+	{
+		return Integer.parseInt(s.charAt(i)+"");
+	}
+
+	/*
+	* Provides the reverse of given string
+	* @param org - The string to be reversed
+	* @return - The reversed string of org
+	*/
+	private static String rev(String org)
+	{
+		String ret = "";
+		for(int i=org.length()-1;i>=0;i--)
+			ret += org.charAt(i)+"";
+		return ret;
+	}
+
+	/*
+	* Trims the leading zeroes of a number
+	* @param str - The number in String form
+	* @return - The number without leading zeroes
+	*/
+	private static String trim(String str)
+	{
+		if(!str.startsWith("0"))
+			return str;
+
+		int i = 0;
+		while(str.charAt(i) == '0')
+			i++;
+		return str.substring(i);
+	}
+
+	/*
+	* Generic print statement
+	* @param e - Any object or data-type to be printed
+	*/
+	static <E> void p(E e)
+	{
+		System.out.println(e);
+	}
 }
