5e39f7607ec052876c665a2378b8e133ff7b8b1c
Sai Kishore <sai2009kishore@gmail.com>
2017-12-21 14:56:39 +0530

    Added support to fractions

--- BigNumbers.java
+++ BigNumbers.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 /**
- * Calculates sum and product of large numbers
- * Created by Sai Kishore on 12/20/2017.
- */
+* Calculates sum and product of large numbers
+* Created by Sai Kishore on 12/20/2017.
+*/
 
-public class BigMultiply
+class BigMultiply
 {
 	// main method
 	public static void main(String args[])
@@ -13,10 +13,47 @@ public class BigMultiply
 		Scanner scanner = new Scanner(System.in);
 		String firstNum = scanner.next();
 		String secondNum = scanner.next();
-		p(multiply(firstNum, secondNum));
+		p(multiplyPoint(firstNum, secondNum));
+	}
+	
+	/**
+	* Multiplies the two numbers digit by digit
+	* @param firstNum - The first number
+	* @param secondNum - the second number
+	* @return - Product of the given numbers
+	*/
+	private static String multiplyPoint(String firstNum, String secondNum)
+	{
+		if(firstNum.contains(".") || secondNum.matches("."))
+		{
+			int decimals = 0;
+			String temp = "";
+			if(firstNum.contains("."))
+			{
+				decimals += firstNum.length() - firstNum.indexOf(".") - 1;
+				for(int i=0;i<firstNum.length();i++)
+					if(firstNum.charAt(i) != '.')
+						temp += firstNum.charAt(i);
+				firstNum = temp;
+			}
+			temp = "";
+			if(secondNum.contains("."))
+			{
+				decimals += secondNum.length() - secondNum.indexOf(".") - 1;
+				for(int i=0;i<secondNum.length();i++)
+					if(secondNum.charAt(i) != '.')
+						temp += secondNum.charAt(i);
+				secondNum = temp;
+			}
+			String result = multiply(firstNum, secondNum);
+			result = result.substring(0, result.length()-decimals) + "." + result.substring(result.length()-decimals);
+			return rev(trim(rev(trim(result))));
+		}
+		else
+			return trim(multiply(firstNum, secondNum));
 	}
 
-	/*
+	/**
 	* Multiplies the two numbers digit by digit
 	* @param firstNum - The first number
 	* @param secondNum - the second number
@@ -72,10 +109,10 @@ public class BigMultiply
 		for(String mul : muls)
 			result = add(result, mul);
 
-		return trim(result);
+		return (result);
 	}
 
-	/*
+	/**
 	* Provides the specified number of zeroes in string form
 	* @param num - The required number of zeroes
 	* @return - The string with num zeroes
@@ -88,7 +125,7 @@ public class BigMultiply
 		return temp;
 	}
 
-	/*
+	/**
 	* Adds two numbers digit by digit
 	* @param a - The first number
 	* @param b - The second number
@@ -134,10 +171,10 @@ public class BigMultiply
 		for(int i=max;i>=0;i--)
 			result += sum[i];
 
-		return trim(result);
+		return (result);
 	}
 
-	/*
+	/**
 	* Converts the char at a particular location in a string to int
 	* @param s - The string whose char is tp be converted
 	* @param i - The position of the character
@@ -148,7 +185,7 @@ public class BigMultiply
 		return Integer.parseInt(s.charAt(i)+"");
 	}
 
-	/*
+	/**
 	* Provides the reverse of given string
 	* @param org - The string to be reversed
 	* @return - The reversed string of org
@@ -161,7 +198,7 @@ public class BigMultiply
 		return ret;
 	}
 
-	/*
+	/**
 	* Trims the leading zeroes of a number
 	* @param str - The number in String form
 	* @return - The number without leading zeroes
@@ -172,12 +209,19 @@ public class BigMultiply
 			return str;
 
 		int i = 0;
-		while(str.charAt(i) == '0')
+		while(i<str.length() && str.charAt(i) == '0')
 			i++;
+		
+		if(i == str.length())
+			return "0";
+		
+		if(str.charAt(i) == '.')
+			return "0"+str.substring(i);
+		
 		return str.substring(i);
 	}
 
-	/*
+	/**
 	* Generic print statement
 	* @param e - Any object or data-type to be printed
 	*/
